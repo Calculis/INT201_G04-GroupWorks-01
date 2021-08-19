d854f070a0bd08c8075fe282a460662b8bceb6b0
Phunon Srivoraluxkana <phunon.sriv@mail.kmutt.ac.th>
2021-08-19 17:25:03 +0700

    variable_declaration

---
+++ Lab01.js
@@ -0,0 +1,13 @@
+var message = "hello";
+console.log(typeof message); //String
+
+var message = 1000;
+console.log(typeof message); //number 
+
+let [member1,member2,member3,member4,member5]=[{'Name':'Non'},{'Name':'Big'}
+,{'Name':'Namwaan'},{'Name':'Prin'},{'Name':'Zom'}];
+console.log(member1);
+
+const Id = [63130500093,63130500061,62130500084,63130500079,63130500095];
+console.log(Id[0]) 
+console.log(typeof Id);//object
